df9b65c1344941c144b901ca8f2a4e245c3a3829
Safwan Marwan <safwanmarwan11@gmail.com>
2015-01-13 09:26:49 +0000

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'testmodel/home'
+  get 'home' => 'testmodel#home'
 
-  get 'testmodel/about'
+  get 'about' => 'testmodel#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
