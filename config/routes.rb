09a37f86207f1d324ba3dc58a68fd6e98e2a3805
Safwan Marwan <safwanmarwan11@gmail.com>
2015-01-13 09:39:24 +0000

    Added StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
   get 'home' => 'testmodel#home'
-  root 'testmodel#home'
+  
   get 'about' => 'testmodel#about'
+  
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
